8d908cc2cf4faae86b288ef9e7fa4ac13d02319e
dipterix <dipterix.wang@gmail.com>
2023-03-27 09:36:02 -0400

    re-compiled two modules

--- modules/surface_reconstruction/make-surface_reconstruction.R
+++ modules/surface_reconstruction/make-surface_reconstruction.R
@@ -1,13 +1,16 @@
 library(targets)
 library(raveio)
 source("common.R", local = TRUE, chdir = TRUE)
-targets::tar_source(
-  sort(list.files(
-    "R/", ignore.case = TRUE,
-    pattern = "^shared-.*\\.R",
-    full.names = TRUE
-  ))
-)
+._._env_._. <- environment()
+lapply(sort(list.files(
+  "R/", ignore.case = TRUE,
+  pattern = "^shared-.*\\.R", 
+  full.names = TRUE
+)), function(f) {
+  source(f, local = ._._env_._., chdir = TRUE)
+})
+targets::tar_option_set(envir = ._._env_._.)
+rm(._._env_._.)
 ...targets <- list(`__Check_settings_file` = targets::tar_target_raw("settings_path", 
     "settings.yaml", format = "file"), `__Load_settings` = targets::tar_target_raw("settings", 
     quote({
